61d60ba8be54d02f735a097f60f1777d9e3ae77f
Iliass b <iliass.bourabaa@usherbrooke.ca>
2023-02-06 13:29:09 -0500

    Butterwork implementation

--- filtre/filtre.h
+++ filtre/filtre.h
@@ -1,9 +1,6 @@
 #ifndef FILTRE_H
 #define FILTRE_H
 
-void dcTrend(double &table, float out[]);
-void rect(double &table, float out[]);
-void lise(double &table, float out[]);
-void filtre(double &input[]);
+void filtre(double *data);
 
 #endif
--- filtre/main.c
+++ filtre/main.c
@@ -4,7 +4,7 @@
 
 int main() {
    
-    filtre();
+    //filtre();
 
     return 0;
 }
